6f0e7c3e857c78304cef9cb19fbec42e26303e25
mihai-radulescu <62609189+mihai-radulescu@users.noreply.github.com>
2021-05-19 12:39:39 +0300

    update

--- AA/Part2/Lab7/ex1.py
+++ AA/Part2/Lab7/ex1.py
@@ -1,3 +1,4 @@
+from math import inf
 
 def readInput(semi:list):
     n = int(input("Nr of semiplanes:\n>> "))
@@ -13,7 +14,37 @@ def main():
     semi = []
     readInput(semi)
     
-    print(semi)
+    maxX = maxY = inf
+    minX = minY = inf
+
+    for plane in semi:
+        upperBound = inf
+        lowerBound = -inf
+
+        if plane[0] == 0:
+            k = plane[1]
+        elif plane[1] == 0:
+            k = plane[0]
+
+        if k > 0:
+            upperBound = -plane[2] / k
+        else:
+            lowerBound = -plane[2] / k
+
+        if plane[0] == 0:
+            minY = max(minY, lowerBound)
+            maxY = min(maxY, upperBound)
+        elif plane[1] == 0:
+            minX = max(minX, lowerBound)
+            maxX = min(maxX, upperBound)
+
+    if minX > maxX or minY > maxY:
+        print("empty")
+    elif (max(maxX, maxY) > inf) or (min(minX, minY) > -inf):
+        print("infinity")
+    else:
+        print("finit")
+        
 
 if __name__ == '__main__':
     main()    
